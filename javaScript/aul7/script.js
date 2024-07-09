13bb0cacb893b9c0d1a5db9dbcc569753db00386
Alekssandher <alekssandher1@hotmail.com>
2024-07-09 09:54:32 -0300

    feita aul 7 - script node js

---
+++ javaScript/aul7/script.js
@@ -0,0 +1,12 @@
+console.log("Hello world");
+
+var num = 10
+var name = "Alek"
+var test = true
+
+console.log(typeof(name))
+
+console.log(test)
+console.log(num)
+
+// Execute o código com node ./script.js - antes verificar se está na pasta correta e com modo sudo 
