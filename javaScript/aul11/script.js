a1f78c6902a184da3b9132e616a505a7b856f0dc
Alekssandher <alekssandher1@hotmail.com>
2024-07-12 10:00:54 -0300

    funções, arrays e objetos

---
+++ javaScript/aul11/script.js
@@ -0,0 +1,15 @@
+var lista = []
+
+
+function createPerson(name, age, sex){
+    let pessoa = [{name, age, sex}]
+    
+    lista.push(pessoa)
+}
+
+createPerson("Alek", 19, "M")
+createPerson("Sid", 11, "m")
+createPerson("Mell", 15, "f")
+
+console.log(lista)
+
