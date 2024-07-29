656c1fdfcff5174ab6f373a4f7b1a7dba841f16e
Alekssandher <alekssandher1@hotmail.com>
2024-07-29 13:34:14 -0300

    cena1

--- atividades/PROJETO/cenas/cena1.js
+++ atividades/PROJETO/cenas/cena1.js
@@ -1,9 +1,42 @@
+const prompt = require('prompt-sync')()
+
 const exibirTexto = require("../sistemas/exibirTexto")
 
-async function acordar(){
+var escolha
+
+async function acordar(fugiu, player){
     console.clear()
-    await exibirTexto("Você acorda num lugar estranho", 10)
     
+
+    if (fugiu) {
+       
+        await exibirTexto("Você sente seu coração batendo forte enquanto tenta entender onde está. As lembranças de sua fuga ainda são frescas na sua mente, você se lembra de cada detalhe, aquela conversa havia sido real demais para ser apenas um pesadelo. O lugar onde acordou é uma clareira cercada por árvores altas e densas. A luz da lua penetra suavemente pelas copas das árvores, criando padrões de sombra no chão. \n", 10);
+        
+
+
+
+    } else {
+        await exibirTexto("Você acorda perto de uma clareira, o lugar ao seu redor parece ser um campo aberto cercado por árvores altas e densas, A luz da lua penetra suavemente pelas copas das árvores, criando padrões de sombra no chão. Você se lembra da mulher misteriosa e suas palavras ainda ecoam na sua mente.", 10);
+    }
+    
+    async function escolher(){
+        
+        await exibirTexto("Você se levanta e se depara com um caminho de pedras a sua frente. \n O que você faz?\n1 - Seguir o caminho de pedras. 2 - Investigar os arredores\n", 10)
+        await (escolha = prompt())
+
+        switch (parseInt(escolha)) {
+            case 1:
+                
+                break;
+            
+            case 2:
+                break
+            default:
+                break;
+        }
+
+    }
+
 }
 
 module.exports = acordar
--- atividades/PROJETO/index.js
+++ atividades/PROJETO/index.js
@@ -10,7 +10,7 @@ var escolha
 var nome
 var player
 
-var correu = false
+var fugiu = false
 async function exibirTextosSequencialmente() {
     console.clear()
     await exibirTexto("Você está diante de uma pessoa misteriosa, você não sabe quem ela é, um véu negro esconde a sua face, porém por sua silhueta você percebe que se trata de uma mulher.  ", 10);
@@ -25,14 +25,14 @@ async function exibirTextosSequencialmente() {
     switch (parseInt(escolha)) {
         case 1:
             console.clear()
-            await exibirTexto("“Você costumava se chamar", 10)
+            await exibirTexto("Você costumava se chamar", 10)
             await exibirTexto("...", 600)
             await exibirTexto(" Bem, não importa mais, isso ficou no passado, agora a ocasião exige que você tenha uma nova identidade, eu poderia escolher o seu nome, mas acho que essa é uma decisão que você deveria tomar sozinho.\n", 10)
             await (nome = prompt("Digite o seu nome: "))
             await criarPlayer(nome)
             
-            await exibirTexto(`Certo, ${player.nome}, acho que isso é tudo por enquanto. Agora é hora de você acordar\n`, 10)
-            await acordar()
+            await exibirTexto(`Certo, ${player.nome}, acho que isso é tudo por enquanto. Agora é hora de você acordar...\n`, 10)
+            await acordar(fugiu, player)
             
 
             break;
@@ -52,7 +52,7 @@ async function exibirTextosSequencialmente() {
 
     async function correu(){
         criarPlayer("Finn")
-        correu = true
+        fugiu = true
 
         await (escolha = prompt())
         
@@ -62,13 +62,14 @@ async function exibirTextosSequencialmente() {
                 await exibirTexto("Você para, se vira e de longe vê aquela moça com as mãos no rosto, exprimindo leves soluços de dor. E então você", 10)
                 await exibirTexto("...", 600)
                 await lembrete()
-                await acordar()
+                await acordar(fugiu, player)
                 break;
             
             case 2:
+                console.clear()
                 await exibirTexto("Você continua correndo com todas as suas forças em direção ao desconhecido, sem nem sequer pensar em olhar para trás.\n", 10)
                 await lembrete()
-                await acordar()
+                await acordar(fugiu, player)
                 
             default:
                 break;
